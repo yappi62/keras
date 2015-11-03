6d7a731305e37aabeec322d059654a78d18a00c9
myungsub <91mschoi@hanmail.net>
2015-11-03 23:30:58 +0900

    Add comments

--- project/jzs1_embed_den_relu_time_soft_pre_adam_cat_cnn.py
+++ project/jzs1_embed_den_relu_time_soft_pre_adam_cat_cnn.py
@@ -7,8 +7,6 @@ from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM, JZS1
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.models import Sequential
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import np_utils, generic_utils
 from keras.callbacks import Callback
 
 import numpy as np
@@ -17,6 +15,7 @@ from datetime import datetime
 from collections import Counter
 import random
 import re
+from preprocess_util import *
 
 # import VQA api
 from vqaTools.vqa import VQA
@@ -25,15 +24,14 @@ from vqaTools.vqa import VQA
 from vgg_16_keras import VGG_16
 
 
-# EACH_LIMIT_SIZE = 10000
-# VAL_EACH_LIMIT_SIZE = 1000
+##### Initialize parameters
+
 LIMIT_SIZE = 10000
 VAL_LIMIT_SIZE = 1000
 EMBED_SIZE = 500
 HIDDEN_SIZE = 500
 BATCH_SIZE = 4
 EPOCHS = 100
-END_MARK = 1
 
 dataDir='../../VQA'
 # print('Enter the taskType (\'OpenEnded\', \'MultipleChoice\')')
@@ -47,80 +45,21 @@ tannFile='%s/Annotations/%s_%s_annotations.json'%(dataDir, dataType, 'val2014')
 tquesFile='%s/Questions/%s_%s_%s_questions.json'%(dataDir, taskType, dataType, 'val2014')
 timgDir = '%s/Images/%s/' %(dataDir, 'val2014')
 
-# initialize VQA api for QA annotations
+
+##### initialize VQA api for QA annotations
+
 vqa=VQA(annFile, quesFile)	# training
 tvqa=VQA(tannFile, tquesFile)	# validation
 
-# load and display QA annotations for given question types
+# QA annotations for given question types
 """
 quesTypes can be one of the following
-what color 
-what kind 
-what are 
-what type 
-is the 
-is this
-how many 
-are 
-does 
-where 
-is there 
-why 
-which 
-do 
-what does 
-what time 
-who 
-what sport 
-what animal 
-what brand
+..
+what color 	what kind 	what are 	what type  	is the
+is this		how many 	are 		does  		where
+is there 	why 		which		do 		what does 
+what time 	who 		what sport 	what animal 	what brand
 """
-
-def tokenize(sent):
-	'''Return the tokens of a sentence including punctuation.
-	>>> tokenize('Bob dropped the apple. Where is the apple?')
-	['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
-	'''
-	return [x.strip().lower() for x in re.split('(\W+)?', sent) if x.strip()]
-
-def get_inputList(vqa, anns, size):
-	data = []
-	limit = 0
-	for i in range(0, len(anns)):
-		ques = tokenize(vqa.qqa[anns[i]['question_id']]['question'])
-		ans = tokenize(anns[i]['multiple_choice_answer'])
-		data += (ques + ans)
-		limit += 1
-		if limit == size:
-			break
-	return data
-
-def get_inputVec(vqa, anns, size):
-	data = []
-	limit = 0
-	for i in range(0, len(anns)):
-		ques = tokenize(vqa.qqa[anns[i]['question_id']]['question'])
-		ans = tokenize(anns[i]['multiple_choice_answer'])
-		data.append((ques, ans))
-		limit += 1
-		if limit == size:
-			break
-	return data
-
-def vectorize(data, size):
-	rX = []
-	rY = []
-	limit = 0
-	for ques, ans in data:
-		x = [word_idx[w] for w in ques]
-		y = [word_idx[w] for w in ans]
-		rX.append(x)
-		rY.append(y+[END_MARK])
-		limit += 1
-		if limit == size :
-			break
-	return pad_sequences(rX, maxlen=ques_maxlen), pad_sequences(rY, maxlen=ans_maxlen)
-
 
 ##### Load VQA dataset
 
@@ -152,69 +91,17 @@ ques_maxlen = max(map(len, (x for x, _ in train + test)))
 ans_maxlen = max(map(len, (x for _, x in train + test))) + 1 # +1 because of adding END_MARK
 
 
+
 ##### Build Train/Test matrices for text QA
 
-aX, aY = vectorize(train, LIMIT_SIZE)
-taX, taY = vectorize(test, VAL_LIMIT_SIZE)
+aX, aY = vectorize(train, LIMIT_SIZE, word_idx, ques_maxlen, ans_maxlen)
+taX, taY = vectorize(test, VAL_LIMIT_SIZE, word_idx, ques_maxlen, ans_maxlen)
 
-X = np.zeros((len(aX), ques_maxlen, vocab_size), dtype=np.bool)
-Y = np.zeros((len(aY), ans_maxlen, vocab_size), dtype=np.bool)
-bY = []
-for i in range(0, len(aX)):
-	start = False
-	for j in range(0, ques_maxlen):
-		if start == False:
-			if aX[i, j] != 0:
-				start = True
-		if start == True:
-			X[i, j, aX[i, j]-1] = 1
-
-
-for i in range(0, len(aY)):
-	start = False
-	code = 0
-	count = 0
-	for j in range(0, ans_maxlen):
-		if start == False:
-			if aY[i, j] != 0:
-				start = True
-		if start == True:
-			Y[i, j, aY[i, j]-1] = 1
-			code += (aY[i, j] * (vocab_size ** (count)))
-			count += 1
-	bY.append(code)
-
-
+X, Y, bY = buildMat(aX, aY, ques_maxlen, ans_maxlen, vocab_size)
 data = Counter(bY)
 accmode = data.most_common(1).pop(0)[1]/float(len(aX))
-tX = np.zeros((len(taX), ques_maxlen, vocab_size), dtype=np.bool)
-tY = np.zeros((len(taY), ans_maxlen, vocab_size), dtype=np.bool)
-tbY = []
-for i in range(0, len(taX)):
-	start = False
-	for j in range(0, ques_maxlen):
-		if start == False:
-			if taX[i, j] != 0:
-				start = True
-		if start == True:
-			tX[i, j, taX[i, j]-1] = 1
 
-
-for i in range(0, len(taY)):
-	start = False
-	code = 0
-	count = 0
-	for j in range(0, ans_maxlen):
-		if start == False:
-			if taY[i, j] != 0:
-				start = True
-		if start == True:
-			tY[i, j, taY[i, j]-1] = 1
-			code += (taY[i, j] * (vocab_size ** (count)))
-			count += 1
-	tbY.append(code)
-
-
+tX, tY, tbY = buildMat(taX, taY, ques_maxlen, ans_maxlen, vocab_size)
 data = Counter(tbY)
 taccmode = data.most_common(1).pop(0)[1]/float(len(taX))
 
@@ -226,27 +113,31 @@ print('ques_maxlen, ans_maxlen = {}, {}'.format(ques_maxlen, ans_maxlen))
 
 print('Build model ...')
 
-##### Load VGGNet (CNN)
-vggnet = VGG_16('vgg16_weights.h5')
+### Load VGGNet (CNN)
+vggnet = VGG_16('vgg16_weights.h5')	# download(553MB) site: https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3 --> weights
 vggnet.add(Dense(4096, HIDDEN_SIZE, activation="relu")) # to match the dimensions of image-question features (in Merge layer)
 
-##### Build Question RNN
+### Build Question RNN
 qnet = Sequential()
 qnet.add(Embedding(vocab_size+1, EMBED_SIZE, mask_zero=True))
 qnet.add(JZS1(EMBED_SIZE, HIDDEN_SIZE))
 
-##### Merged model
+### Merged model
 model = Sequential()
 model.add(Merge([vggnet, qnet], mode="concat", concat_axis=1)) # output_dim = 2*HIDDEN_SIZE
 #model.add(Dense(2*HIDDEN_SIZE, HIDDEN_SIZE, activation="relu"))
 model.add(RepeatVector(ans_maxlen))
 model.add(JZS1(2*HIDDEN_SIZE, HIDDEN_SIZE, return_sequences=True))
 model.add(TimeDistributedDense(HIDDEN_SIZE, vocab_size, activation="softmax")) # TimeDistributedDense
-# model.add(Activation('softmax')) # time_distributed_softmax
 
+print('Model compiling ...')
 opt = Adam(lr = 0.000125)
-
 model.compile(optimizer=opt, loss='categorical_crossentropy') # mean_squared_error, categorical_crossentropy
+
+
+
+
+##### Training (+logging)
 
 print('Training ...')
 fResult = open('jzs1_embed_den_relu_time_soft_pre_adam_cat.txt', 'w')
